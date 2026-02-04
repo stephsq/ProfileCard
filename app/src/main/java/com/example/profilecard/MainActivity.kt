4ddb3921d86a88f09353ff7c8b8a15b3ebd29a85
stephsq <stephsquach@gmail.com>
2026-02-04 13:04:36 -0500

    Comments added, need to figure out why my background color does not work in Surface(), but works in Column

--- app/src/main/java/com/example/profilecard/MainActivity.kt
+++ app/src/main/java/com/example/profilecard/MainActivity.kt
@@ -12,14 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.profilecard.ui.theme.ProfileCardTheme
@@ -35,50 +33,51 @@ class MainActivity : ComponentActivity() {
             ProfileCardTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
+                    color = Color(0xFFF6EDFF) //background color does not work here for some reason
+                )
+                {
+                    ProfileCard(
+                        name = "Stephanie Quach",
+                        description = "Hi! My name is Stephanie and I'm a programming student at Pellissippi College.",
+                    )
                 }
             }
         }
     }
 }
 
+//profile card layout
 @Composable
 fun ProfileCard(
     name: String,
     description: String,
-    backgroundColor: Color,
     modifier: Modifier = Modifier
 ) {
-
+    //column to stack components vertically
     Column (
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFFA8A7F7))
+            .background(Color(0xFFA8A7F7)) //background color works here, but not the surface
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        ProfileImage()
+        ProfileImage() //displays image
         Text(
             text = name,
             fontSize = 40.sp,
-            lineHeight = 50.sp,
             modifier = modifier
-                .padding(8.dp)
         )
         Text(
             text = description,
             fontSize = 20.sp,
-            lineHeight = 30.sp,
             modifier = modifier
-                .padding(8.dp)
         )
     }
 }
 
+//display profile image as a circle
 @Composable
 fun ProfileImage(modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.profilepicture)
@@ -87,21 +86,20 @@ fun ProfileImage(modifier: Modifier = Modifier) {
         contentDescription = "Profile Picture",
         modifier = modifier
             .clip(CircleShape)
-            .padding(8.dp)
-
-
+            .size(300.dp)
     )
-
-
 }
 
+//preview for testing UI
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ProfileCardPreview() {
     ProfileCardTheme {
-        ProfileCard(
-            name = "Stephanie Quach",
-            description = "Hi! my name is Stephanie and I'm a programmer.",
-            backgroundColor = Color(0xFFF6EDFF))
+
+            ProfileCard(
+                name = "Stephanie Quach",
+                description = "Hi! My name is Stephanie and I'm a programming student at Pellissippi College."
+            )
+
     }
 }
